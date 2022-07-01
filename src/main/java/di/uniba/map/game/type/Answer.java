45a02ceae7af7e03b9fbadfb02afe022fc9fcc35
Giuliorusso01 <100955104+Giuliorusso01@users.noreply.github.com>
2022-07-01 12:28:34 +0200

    javadoc per Type/Answer.java

--- src/main/java/di/uniba/map/game/type/Answer.java
+++ src/main/java/di/uniba/map/game/type/Answer.java
@@ -1,24 +1,69 @@
 package di.uniba.map.game.type;
 
+/**
+ * @author Giulio Russo
+ * All'interno di questa classe vengono gestite le risposte date all'interno dei vari dialoghi.
+ */
+
 public class Answer {
     private String answer = new String();
 
     private Talk warp = null;
 
     private Triggers.triggerInterface triggerReference = null;
+    
+     /** 
+     * @return String
+     */
 
-    //insert trigger to answer
+    public String getAnswer()
+    {
+        return answer;
+    }
 
-    public String getAnswer(){return answer;}
+    /** 
+     * @param answer
+     */
+    
+    public void setAnswer(String answer)
+    {
+        this.answer = answer;
+    }
 
-    public void setAnswer(String answer){this.answer = answer;}
+    /** 
+     * @return Talk
+     */
+    
+    public Talk getWarp()
+    {
+        return warp;
+    }
 
-    public Talk getWarp(){return warp;}
+    
+    /** 
+     * @param warp
+     */ 
 
-    public void setWarp(Talk warp){this.warp = warp;}
+    public void setWarp(Talk warp)
+    {
+        this.warp = warp;
+    }
 
-    public void setTriggerReference(Triggers.triggerInterface T){this.triggerReference = T;}
+    /** 
+     * @param T
+     */
+    
+    
+    public void setTriggerReference(Triggers.triggerInterface T)
+    {
+        this.triggerReference = T;
+    }
 
+    
+    /** 
+     * @return triggerInterface
+     */
+    
     public Triggers.triggerInterface getTriggerReference(){
         return this.triggerReference;
     }
