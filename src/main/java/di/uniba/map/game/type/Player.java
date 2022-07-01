b51ddcc5278759114086e255d009dfd2221e3564
Frank-sudoapt <54440168+Frank-sudoapt@users.noreply.github.com>
2022-07-01 12:41:31 +0200

    javadoc per Item/Player.java

--- src/main/java/di/uniba/map/game/type/Player.java
+++ src/main/java/di/uniba/map/game/type/Player.java
@@ -1,20 +1,44 @@
 package di.uniba.map.game.type;
 
+/**
+ * @author Francesco Pio Scoglietti
+ * All'interno di questa classe vengono definite le caratteristiche del protagonista
+ */
 public class Player extends Character{
 
     private Inventory inventory = new Inventory();
 
     protected Item clothesEquip;
 
-    public Player(int hp, String name, String description) {
+    public Player(int hp, String name, String description) 
+    {
         super(hp, name, description);
     }
 
-    public Inventory getInventory(){ return inventory;}
+    
+    /** 
+     * @return Inventory
+     */
+    public Inventory getInventory()
+    { 
+        return inventory;
+    }
 
-    public Item getClothesEquip() { return clothesEquip;}
+    
+    /** 
+     * @return Item
+     */
+    public Item getClothesEquip() 
+    {
+        return clothesEquip;
+    }
 
-    public void setClothesEquip(Item clothesEquip){
+    
+    /** 
+     * @param clothesEquip
+     */
+    public void setClothesEquip(Item clothesEquip)
+    {
         this.clothesEquip = clothesEquip;
     }
 
