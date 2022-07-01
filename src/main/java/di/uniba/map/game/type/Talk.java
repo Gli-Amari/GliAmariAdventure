d64f1dfd57e39c6befd9532e10514af546010e89
Frank-sudoapt <54440168+Frank-sudoapt@users.noreply.github.com>
2022-07-01 12:42:27 +0200

    javadoc per Item/Talk.java

--- src/main/java/di/uniba/map/game/type/Talk.java
+++ src/main/java/di/uniba/map/game/type/Talk.java
@@ -3,19 +3,39 @@ package di.uniba.map.game.type;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Francesco Pio Scoglietti
+ * In questa classe vengono definiti i dialoghi possibili con i personaggi interagibili!
+ */
 public class Talk {
 
     private String speech = new String();
-
     private List<Answer> ans = new ArrayList<>();
 
-    public String getSpeech() {
+    
+    /** 
+     * @return String
+     */
+    public String getSpeech() 
+    {
         return speech;
     }
 
-    public void setSpeech(String s){
+    
+    /** 
+     * @param s
+     */
+    public void setSpeech(String s)
+    {
         speech = s;
     }
 
-    public List<Answer> getAns() {return ans;}
+    
+    /** 
+     * @return List<Answer>
+     */
+    public List<Answer> getAns() 
+    {
+        return ans;
+    }
 }
