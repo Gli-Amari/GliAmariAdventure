b629141fedfc013ce6bdd2e84fbdb90094718519
Piersilvio <p.spicoli@studenti.uniba.it>
2022-07-11 15:28:10 +0200

    committtt

--- src/main/java/di/uniba/map/game/type/Npc.java
+++ src/main/java/di/uniba/map/game/type/Npc.java
@@ -5,8 +5,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 import di.uniba.map.game.databases.Db;
 
